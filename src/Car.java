200ba23a7495746f2143ea1cb53289f0d97472c7
Noahnom <gusfawom@student.gu.se>
2018-01-29 12:26:12 +0100

    project main structure

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    int[] carPosition = new int[2];
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void leftLaneDetect() {
+        
+    }
+}
