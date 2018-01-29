98581c2fc50b0c12d522747b8e0c985418edd0c5
gussinku <gussinku@student.gu.se>
2018-01-29 13:35:20 +0100

    added changelane method

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,7 @@
 public class Car {
     int[] carPosition = new int[2];
 
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
@@ -8,4 +9,21 @@ public class Car {
     public void leftLaneDetect() {
         
     }
+
+    /*public int changeLane( int pos ){
+
+        if (leftLaneDetect()){
+            System.out.print("Lane change successfully performed");
+            //do something
+            moveForward();
+            changeLane();
+        }
+        else{
+            System.out.print("Incoming car detected, stay on the same lane");
+            //do something
+            moveForward();
+    }
+         return carPosition;
+
+    }*/
 }
