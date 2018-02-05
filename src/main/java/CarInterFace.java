ec2ed15290c7bf4328f05fa563b41fb74713d6b0
Noahnom <gusfawom@student.gu.se>
2018-02-05 11:29:15 +0100

    added more info to interface

--- src/main/java/CarInterFace.java
+++ src/main/java/CarInterFace.java
@@ -17,12 +17,13 @@ public interface CarInterFace {
 	/*
 	  Description: Detects if a car is nearby (0-5 meters away) using 3 radars and 1 lidar. Queries itself
 	  twice to revise its readings. Alerts the user if their is more than 1 faulty sensor reading as well.
-
-	  Pre-condition: All sensors return a reading to be analyzed. 
+	  Only one reading between 0-5 meters is needed for the car to alert that another car was detected.
+		
+	  Pre-condition: All sensors give a reading to be analyzed. 
 
 	  Post-condition: Returns if a car is detected, not detected, or if their are faulty sensor readings.
 	  
 	  Test-cases:
 	*/
-	public String  leftLaneDetect(int radars[], int lidar, int query);
+	public String  leftLaneDetect(int radars[], int lidar, int query); 
 }
