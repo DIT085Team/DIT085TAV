ff8f41fbd574f2988c474030b4ac8ab15aaf4f2d
Cypher <gusrashisa@student.gu.se>
2018-02-19 22:38:49 +0100

    comments

--- src/main/java/controller/Car.java
+++ src/main/java/controller/Car.java
@@ -11,7 +11,7 @@ public interface Car {
 	/*Description : The car moves five meters until it has 
 	  reached 100meter.
 	  
-	  Precondition: The y position of the car.
+	  Precondition: The position of the car has to be initialized.
 	  
 	  Postcondition: The position.
 	  
