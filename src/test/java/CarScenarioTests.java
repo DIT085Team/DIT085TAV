ec34f6a9b22145a843f4c16c8d263905e05ab5d4
Noahnom <gusfawom@student.gu.se>
2018-02-19 15:15:05 +0100

    scenario class template

---
+++ src/test/java/CarScenarioTests.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import controller.Actuator;
+import controller.CarImp;
+
+public class CarScenarioTests {
+	private CarImp auto;
+	
+	@Mock
+	Actuator act = mock(Actuator.class);
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
